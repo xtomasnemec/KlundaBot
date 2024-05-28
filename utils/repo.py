fa86684f962d3218ff123592e0067440c4d28d58
silver_volt4 <Silver-Volt4@users.noreply.github.com>
2024-05-28 17:38:08 +0200

    damn you unix

--- utils/repo.py
+++ utils/repo.py
@@ -10,7 +10,7 @@ def get_output(command, *args, **kwargs):
 
 
 def update():
-    assert subprocess.run("git pull origin main").returncode == 0
+    assert subprocess.run(["git", "pull", "origin", "main"]).returncode == 0
 
 
 def create_data_clone():
@@ -25,8 +25,7 @@ def create_data_clone():
                 REMOTE_URL,
                 ASSET_DIRECTORY_NAME,
             ],
-            cwd=BOT_DIRECTORY,
-            shell=True,
+            cwd=BOT_DIRECTORY
         ).returncode
         == 0
     )
@@ -38,8 +37,7 @@ def create_data_clone():
                 "user.name",
                 "SilverBot",
             ],
-            cwd=ASSET_DIRECTORY,
-            shell=True,
+            cwd=ASSET_DIRECTORY
         ).returncode
         == 0
     )
@@ -51,8 +49,7 @@ def create_data_clone():
                 "user.email",
                 "",
             ],
-            cwd=ASSET_DIRECTORY,
-            shell=True,
+            cwd=ASSET_DIRECTORY
         ).returncode
         == 0
     )
@@ -66,29 +63,28 @@ def add_asset(file, path: str):
     f.close()
     assert (
         subprocess.run(
-            ["git", "add", str(path)], cwd=ASSET_DIRECTORY, shell=True
+            ["git", "add", str(path)], cwd=ASSET_DIRECTORY
         ).returncode
         == 0
     )
     assert (
         subprocess.run(
             ["git", "commit", "-m", "auto: Upload asset"],
-            cwd=ASSET_DIRECTORY,
-            shell=True,
+            cwd=ASSET_DIRECTORY
         ).returncode
         == 0
     )
     assert (
         subprocess.run(
-            [f"git", "push", "origin", "assets"], cwd=ASSET_DIRECTORY, shell=True
+            ["git", "push", "origin", "assets"], cwd=ASSET_DIRECTORY
         ).returncode
         == 0
     )
 
 
 ASSET_DIRECTORY_NAME = "silverbot_data"
-REMOTE_URL = get_output("git remote get-url origin")
-BOT_DIRECTORY = Path(get_output("git rev-parse --show-toplevel"))
+REMOTE_URL = get_output(["git", "remote", "get-url", "origin"])
+BOT_DIRECTORY = Path(get_output(["git", "rev-parse", "--show-toplevel"]))
 ASSET_DIRECTORY = BOT_DIRECTORY / ASSET_DIRECTORY_NAME
 if not ASSET_DIRECTORY.exists():
     print("Asset directory not found, cloning and setting it up for you.")
