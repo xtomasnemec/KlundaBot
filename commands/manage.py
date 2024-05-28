86c0d4b7846a58848e986d124a634e1f525a2c5d
Silver-Volt4 <35656715+Silver-Volt4@users.noreply.github.com>
2024-05-28 17:23:05 +0200

    actually no, screw submodules! here's whatever i just made

--- commands/manage.py
+++ commands/manage.py
@@ -14,6 +14,7 @@ from discord.commands import Option
 
 from db.pet import Character
 from utils.error_message import apologize
+from utils import repo
 
 admin_guild_id = int(os.environ.get("admin_guild_id", "0"))
 
@@ -40,7 +41,7 @@ class Manage(commands.Cog):
         checks=[commands.is_owner().predicate],
         guild_ids=[admin_guild_id],
     )
-    
+
     def reload_cogs(self):
         for cog in [x[:-3] for x in os.listdir("commands") if x.endswith(".py")]:
             try:
@@ -69,7 +70,7 @@ class Manage(commands.Cog):
         """
         await self.bot.sync_commands()
         await ctx.respond("✅ Done", ephemeral=True)
-    
+
     @admin.command(descriptioon="Sync commands to a guild")
     async def refreshall(self, ctx: discord.ApplicationContext):
         """
@@ -87,22 +88,21 @@ class Manage(commands.Cog):
         name: Option(str, "Character name"),
         is_oc: Option(bool, "Whether this is an OC"),
         owner: Option(str, "Owner of this OC (if applicable)", required=False),
-        url: Option(str, "Image URL to download and automatically commit", required=False),
+        url: Option(
+            str, "Image URL to download and automatically commit", required=False
+        ),
     ):
         if is_oc and not owner:
-            await ctx.respond("❌ An owner must be supplied if adding an OC", ephemeral=True)
+            await ctx.respond(
+                "❌ An owner must be supplied if adding an OC", ephemeral=True
+            )
         if url:
-            outfile = f'assets/pet/{"oc" if is_oc else "character"}/{gifname}.gif'
+            outfile = f'pet/{"oc" if is_oc else "character"}/{gifname}.gif'
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as resp:
                     if resp.status == 200:
-                        f = open(outfile, mode='wb')
-                        f.write(await resp.read())
-                        f.close()
-            assert os.system(f'git reset') == 0
-            assert os.system(f'git add "{outfile}"') == 0
-            assert os.system(f'git commit "{outfile}" -m "Upload .gif, automatic commit"') == 0
-            assert os.system(f'git push origin main') == 0
+                        repo.add_asset(await resp.read(), outfile)
+
         Character.create(gif=gifname, name=name, is_oc=is_oc, owner=owner)
         # pylint: disable=protected-access
         self.bot._pending_application_commands = []
@@ -110,15 +110,16 @@ class Manage(commands.Cog):
         self.bot.load_extension("commands.pet")
         await self.bot.sync_commands()
         await ctx.respond("✅ Done", ephemeral=True)
-    
+
     @admin.command(description="Update from GitHub")
     async def pull(self, ctx: discord.ApplicationContext):
         """
         Update from GitHub.
         """
-        assert os.system(f'git pull origin main') == 0
+        await ctx.defer()
+        repo.update()
         await ctx.respond("✅ Done", ephemeral=True)
-    
+
     @commands.Cog.listener()
     async def on_application_command_error(
         self, ctx: discord.ApplicationContext, error: discord.DiscordException
